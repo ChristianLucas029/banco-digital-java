9b311768d72978846b6c0e131526ec6118de773f
christianlucas029 <lucasntc29@gmail.com>
2024-08-17 10:01:49 -0300

    Adicionando novas classes

--- src/Agencia.java
+++ src/Agencia.java
@@ -1,4 +1,45 @@
+import java.util.List;
 
 public class Agencia {
 
+    private int numero;
+    private String nome;
+    private List<Conta> contas;
+
+    public Agencia(int numero, String nome) {
+        this.numero = numero;
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
+    }
+
+    public void adicionarConta(Conta conta) {
+        this.contas.add(conta);
+    }
+
+    public void removerConta(Conta conta) {
+        this.contas.remove(conta);
+    }
 }
--- src/Gerente.java
+++ src/Gerente.java
@@ -1,4 +1,45 @@
+import java.util.List;
 
 public class Gerente {
 
+    private String nome;
+    private String id;
+    private List<Cliente> clientes;
+
+    public Gerente(String nome, String id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public void adicionarCliente(Cliente cliente) {
+        this.clientes.add(cliente);
+    }
+
+    public void removerCliente(Cliente cliente) {
+        this.clientes.remove(cliente);
+    }
 }
